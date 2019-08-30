127cb138d1885bf21878fd1606838e3acc6d7046
Ekarin Pongpipat <epongpipat@gmail.com>
2019-08-30 15:53:04 -0500

    include Rproj file and update binary prob func

--- R/predict_binary_probability.R
+++ R/predict_binary_probability.R
@@ -1,13 +1,26 @@
 predict_binary_probability <- function(model, x) {
-  # model <- df_pred$model[[2]]
+
+  # determine class (model type)
   class <- model %>% attributes %>% .$class
-  # str_detect(class, "glm")
+
+  # elastic net
   if (any(str_detect(class, "glmnet"))) {
-    predict <- predict.glmnet(model, as.matrix(x))
+    predict <- predict(df_model$model_lasso[[1]], df_model$data_x[[1]], type = "response") %>% as_tibble()
+
+  # svm
   } else if (any(str_detect(class, "svm"))) {
-    predict <- predict(model)
+    predict <- predict(df_model$model_svm_linear[[1]], df_model$data[[1]], probability = TRUE) %>%
+      attributes() %>%
+      .$probabilities %>%
+      as_tibble()
+
+  # glm
   } else if (any(str_detect(class, "glm"))) {
     predict <- predict.glm(model)
+
+  # not supported
+  } else {
+    stop(paste0("Class (", class, ") is not supported"))
   }
 
   predict %>% unlist() %>% as.numeric()
