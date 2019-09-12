a814c7a8848588b0771723753ba116e8d57950bc
Ekarin Pongpipat <epongpipat@gmail.com>
2019-09-12 17:09:24 -0500

    Update tidy_colnames.R
    - added more character replacements

--- R/tidy_colnames.R
+++ R/tidy_colnames.R
@@ -2,12 +2,35 @@
 # 1. lower-case
 # 2. replaces:
 #    - periods with underscores
+#    - space with underscores
+#    - double underscore with one underscore
+#    - parentheses with underscore
+#    - brackets with underscore
+#    - forward and backward slashes with underscore
+#    - dash with underscore
+#    - e_mail with email
+#    - removes trailing white space and trailing underscores
 tidy_colnames <- function(data) {
   require(xfun)
   packages <- c("dplyr", "stringr")
   xfun::pkg_attach(packages, install = T, message = F)
   colnames(data) <- colnames(data) %>%
-    tolower() %>%                           
-    str_replace_all(., "[.]", "_")          
+    tolower() %>%
+    str_replace_all(., "[.]", "_") %>%
+    str_replace_all(., "[ ]", "_") %>%
+    str_replace_all(., "\\(", "_") %>%
+    str_replace_all(., "\\)", "_") %>%
+    str_replace_all(., "\\[", "_") %>%
+    str_replace_all(., "\\]", "_") %>%
+    str_replace_all(., "/", "_") %>%
+    str_replace_all(., "\\\\", "_") %>%
+    str_replace_all(., "_$", "") %>%
+    str_replace_all(., "-", "_") %>%
+    str_replace_all(., "e_mail", "email") %>%
+    str_replace_all(., "__", "_") %>%
+    trimws()
+
+  #colnames(data) <- sub("_$", "", colnames(data))
+
   return(data)
 }
