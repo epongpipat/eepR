262895444687c351c477e5497969ac8d2b961013
Ekarin Pongpipat <epongpipat@gmail.com>
2019-10-16 15:49:09 -0500

    update tidy colnames

--- R/tidy_colnames.R
+++ R/tidy_colnames.R
@@ -1,19 +1,36 @@
-# currently, does the following modifications:
-# 1. lower-case
-# 2. replaces:
-#    - periods with underscores
-#    - space with underscores
-#    - double underscore with one underscore
-#    - parentheses with underscore
-#    - brackets with underscore
-#    - forward and backward slashes with underscore
-#    - dash with underscore
-#    - e_mail with email
-#    - removes trailing white space and trailing underscores
+#' @title tidy_colnames
+#' @description makes the data.frame have column names that are in tidy format.
+#' @details currently, does the following modifications:
+#' 1. lower-case
+#' 2. replaces:
+#'    - periods with underscores
+#'    - space with underscores
+#'    - double underscore with one underscore
+#'    - parentheses with underscore
+#'    - brackets with underscore
+#'    - forward and backward slashes with underscore
+#'    - dash with underscore
+#'    - e_mail with email
+#'    - removes trailing white space and trailing underscores
+#'
+#' @param data data.frame that contains the column names to be converted
+#' @importFrom xfun pkg_attach
+#' @import dplyr stringr
+#' @return data.frame with tidy column names
+#' @export
+#'
+#' @examples
+#' # before
+#' colnames(carData::Salaries)
+#'
+#' # after
+#' df <- carData::Salaries %>%
+#' tidy_colnames(.)
+#' colnames(df)
 tidy_colnames <- function(data) {
-  require(xfun)
-  packages <- c("dplyr", "stringr")
-  xfun::pkg_attach(packages, install = T, message = F)
+  #require(xfun)
+  #packages <- c("dplyr", "stringr")
+  #xfun::pkg_attach(packages, install = T, message = F)
   colnames(data) <- colnames(data) %>%
     tolower() %>%
     str_replace_all(., "[.]", "_") %>%
