86ba7807231c303018fad32dcdfc3f45d03d858a
Ekarin Eric Pongpipat <epongpipat@users.noreply.github.com>
2019-10-16 23:09:18 -0500

    update append suffix to colnamea

--- R/append_suffix_to_colnames.R
+++ R/append_suffix_to_colnames.R
@@ -1,12 +1,13 @@
-#' append_suffix_to_colnames
+#' @title append_suffix_to_colnames
+#' @description add a string to end of all the column names
+#' @param data data.frame that contains the column names to change with a suffix
+#' @param suffix string to add to the end of all of the column names of the data.frame
 #'
-#' @param data
-#' @param suffix
-#'
-#' @return
+#' @return data.frame with new column names with suffix string appended to the column names
 #' @export
 #'
 #' @examples
+#' # to be added
 append_suffix_to_colnames <- function(data, suffix) {
   colnames(data) <- paste0(colnames(data), "_", suffix)
   return(data)
