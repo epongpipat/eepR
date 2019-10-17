6c3706b9291ff913349d8c3fc617f791e80dd24f
Ekarin Pongpipat <epongpipat@gmail.com>
2019-10-17 12:44:54 -0500

    remove tictoc from elastic net models

--- R/model_binary_elastic_net.R
+++ R/model_binary_elastic_net.R
@@ -22,18 +22,14 @@ model_binary_elastic_net <- function(data, y) {
     data.frame(cvm = cv$cvm[cv$lambda == cv$lambda.1se], lambda.1se = cv$lambda.1se, alpha = a)
   }
 
-  tic()
   df_model <- tibble(a) %>%
     .[1:100,] %>%
     mutate(cv_model = future_map(a, func_cv))
-  toc()
 
-  tic()
   search <- foreach(i = a[1:100], .combine = rbind) %dopar% {
     cv <- cv.glmnet(x, y, family = "binomial", nfold = 10, parallel = TRUE, alpha = i)
     data.frame(cvm = cv$cvm[cv$lambda == cv$lambda.1se], lambda.1se = cv$lambda.1se, alpha = i)
   }
-  toc()
 
   cv_elastic_net <- search[search$cvm == min(search$cvm), ]
   glmnet(x, y, family = "binomial", lambda = cv_elastic_net$lambda.1se, alpha = cv_elastic_net$alpha)
