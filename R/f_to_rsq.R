468515899482dbd56826108149c2f0cfe59e40c3
Ekarin Pongpipat <epongpipat@gmail.com>
2019-12-04 10:26:56 -0600

    update model binary ridge man and help page

---
+++ R/f_to_rsq.R
@@ -0,0 +1,3 @@
+f_to_rsq <- function(f, df, df_resid) {
+
+}
--- R/model_binary_ridge.R
+++ R/model_binary_ridge.R
@@ -1,7 +1,13 @@
-require(xfun)
-packages <- c("glmnet", "dplyr")
-xfun::pkg_attach(packages, message = F, install = T)
-
+#' @title model_binary_ridge
+#' @description predict binary outcome using ridge regression
+#' @param data clean data.frame of all variables to be used
+#' @param y name of binary outcome within the data.frame to be predicted
+#'
+#' @return ridge model results
+#' @export
+#' @import glmnet dplyr
+#' @examples
+#' # to be added
 model_binary_ridge <- function(data, y) {
   x <- data %>% select(-y) %>% as.matrix()
   y <- data %>% select(y) %>% as.matrix()
