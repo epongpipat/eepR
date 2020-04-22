d23ff31c2e1b032b5ed09b600d38f9324193a6b6
epongpipat <epongpipat@gmail.com>
2020-04-22 14:18:02 -0500

    fix sprintf cmd

--- R/stop_if_dne.R
+++ R/stop_if_dne.R
@@ -8,6 +8,6 @@
 #' @examples
 stop_if_dne <- function(file) {
   if (!file.exists(file)) {
-    stop(sprintf("file (%s) does not exist"), file)
+    stop(sprintf("file (%s) does not exist", file))
   }
 }
--- R/stop_if_e.R
+++ R/stop_if_e.R
@@ -8,6 +8,6 @@
 #' @examples
 stop_if_e <- function(file) {
   if (file.exists(file)) {
-    stop(sprintf("file (%s) already exists"), file)
+    stop(sprintf("file (%s) already exists", file))
   }
 }
