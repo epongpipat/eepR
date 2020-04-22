e30ea8a1ea32a1530dbfb43ee2e7aae63a2fa6d4
epongpipat <epongpipat@gmail.com>
2020-04-22 14:10:42 -0500

    add roxygen to funcs

--- R/stop_if_dne.R
+++ R/stop_if_dne.R
@@ -1,3 +1,11 @@
+#' stop_if_dne
+#'
+#' @param file
+#'
+#' @return
+#' @export
+#'
+#' @examples
 stop_if_dne <- function(file) {
   if (!file.exists(file)) {
     stop(sprintf("file (%s) does not exist"), file)
--- R/stop_if_e.R
+++ R/stop_if_e.R
@@ -1,3 +1,11 @@
+#' stop_if_e
+#'
+#' @param file
+#'
+#' @return
+#' @export
+#'
+#' @examples
 stop_if_e <- function(file) {
   if (file.exists(file)) {
     stop(sprintf("file (%s) already exists"), file)
