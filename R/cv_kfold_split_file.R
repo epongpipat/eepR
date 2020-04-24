e4defaf5c9f80c1a836ad32603f9fdd6e9e87e9c
epongpipat <epongpipat@gmail.com>
2020-04-24 00:46:27 -0500

    Update cv_kfold_split_file.R
    add mkdir so that it will properly copy original files

--- R/cv_kfold_split_file.R
+++ R/cv_kfold_split_file.R
@@ -25,10 +25,12 @@ cv_kfold_split_file <- function(x_path, y_path, k_fold, out_dir) {
   }
 
   out_file <- glue("{out_dir}/x.csv")
+  mkdir_if_dne(out_file)
   stop_if_e(out_file)
   file.copy(x_path, out_file)
 
   out_file <- glue("{out_dir}/y.csv")
+  mkdir_if_dne(out_file)
   stop_if_e(out_file)
   file.copy(y_path, out_file)
 
