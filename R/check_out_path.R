24f342da2f9344e8349ec5c8a00acfd88a127759
epongpipat <epongpipat@gmail.com>
2021-06-09 23:15:11 -0500

    update check out path to include warn only option and remove skip option

--- R/check_out_path.R
+++ R/check_out_path.R
@@ -2,26 +2,25 @@
 #' @description \preformatted{
 #' checks to see if output file exists.
 #'
-#' 1. STOP WITH ERROR if output file exists, overwrite set to FALSE and skip to FALSE
-#' 2. WARN AND SKIP to next in the for/while loop if output file exists, overwrite set to FALSE, and skip set to TRUE
+#' 1. STOP WITH ERROR if output file exists, overwrite set to FALSE and warn_only set to FALSE
+#' 2. WARN if output file exists, overwrite set to FALSE and warn_only set to TRUE
 #' 3. WARN if output file exists and overwrite set to TRUE
 #' }
 #' @concept r_helper
 #' @param out_path path to output file
 #' @param overwrite booleon to overwrite the output path (default: FALSE)
-#' @param skip boolean to skip if used in a for/while loop (default: FALSE)
+#' @param warn_only boolean to warn instead of stop if file exists and overwrite set to FALSE (default: FALSE)
 #'
 #' @return
 #' @importFrom glue glue
 #' @export
 #' @examples
 #'
-check_out_path <- function(out_path, overwrite = FALSE, skip = FALSE) {
-  if (file.exists(out_path) & overwrite == FALSE & skip == FALSE) {
+check_out_path <- function(out_path, overwrite = FALSE, warn_only = FALSE) {
+  if (file.exists(out_path) & overwrite == FALSE & warn_only == FALSE) {
     stop(glue('output file already exists and overwrite set to FALSE: {out_path}'))
-  } else if (file.exists(out_path) & overwrite == FALSE & skip == TRUE) {
-    warning(glue('skipping, output file already exists and overwrite set to FALSE: {out_path}'))
-    next()
+  } else if (file.exists(out_path) & overwrite == FALSE & warn_only == TRUE) {
+    warning(glue('output file already exists and overwrite set to FALSE: {out_path}'))
   } else if (file.exists(out_path) & overwrite == TRUE) {
     warning(glue('overwriting, output file already exists and overwrite set to TRUE: {out_path}'))
   }
