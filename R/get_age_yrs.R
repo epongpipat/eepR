90c950021f994502d0450090ea62eb95c3456a21
Ekarin Eric Pongpipat <epongpipat@gmail.com>
2026-02-17 23:55:50 -0600

    new fun: get_age_yrs

---
+++ R/get_age_yrs.R
@@ -0,0 +1,15 @@
+#' get_age_yrs
+#'
+#' @param dob date of birth
+#' @param ref_date reference date (default: today's date)
+#'
+#' @returns
+#' @export
+#' @importFrom lubridate today
+#' @importFrom lubridate interval
+#' @importFrom lubridate dyears
+#' @examples
+get_age_yrs <- function(dob, ref_date = lubridate::today()) {
+  return(round(lubridate::interval(dob, ref_date) / lubridate::dyears(), 4))
+}
+
