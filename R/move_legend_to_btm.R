f39cfb7b24ca9592e954ce258f8e3768f018a0d7
Ekarin Eric Pongpipat <epongpipat@gmail.com>
2026-02-17 23:53:33 -0600

    add fun: move_legend_to_btm

---
+++ R/move_legend_to_btm.R
@@ -0,0 +1,25 @@
+#' move_legend_to_btm
+#'
+#' @returns
+#' @export
+#'
+#' @examples
+move_legend_to_btm <- function(learn = FALSE) {
+  cmd='theme(
+    legend.position = "bottom",
+    legend.title.position = "top",
+    legend.title.align = 0.5
+  )'
+  if (learn) {
+    # cat('[CMD]\n')
+    cat(cmd, '\n')
+  }
+  theme(
+    legend.position = "bottom",
+    legend.title.position = "top",
+    legend.title.align = 0.5
+  )
+  # eval(cmd)
+}
+
+
