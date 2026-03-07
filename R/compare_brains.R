32e2196581584381109b529176c26f950f0d70c6
Ekarin Eric Pongpipat <epongpipat@gmail.com>
2026-03-07 00:52:54 -0600

    update default bg and text color

--- R/compare_brains.R
+++ R/compare_brains.R
@@ -5,15 +5,15 @@
 #' @param b second image to compare
 #' @param mask mask image
 #' @param ci confidence interval for bland-altman plots (default: 0.95)
-#' @param legend_title
+#' @param legend_title legend title (default: intensity)
 #' @param out_path path to save image
 #' @param out_width width of saved image in inches (default: 11)
 #' @param out_height height of saved image in inches (default: 8.5)
-#' @param ... additional parameters for `plot_brain()`
-#' @return
+#' @param ... additional parameters for [plot_brain()]
+#' @return ggplot figure comparing two images
 #' @export
 #' @import patchwork
-compare_brains <- function(a, b, mask, ci = 0.95, legend_title = NULL, out_path = NULL, out_width = 14, out_height = 7, ...) {
+compare_brains <- function(a, b, mask, ci = 0.95, legend_title = 'Intensity', out_path = NULL, out_width = 14, out_height = 7, ...) {
 
   imgs <- list()
   imgs$a <- a
@@ -27,9 +27,9 @@ compare_brains <- function(a, b, mask, ci = 0.95, legend_title = NULL, out_path 
            mean = (a + b) / 2)
 
   p <- list()
-  p$a <- plot_brain(imgs$a, legend_title = legend_title, ...)
+  p$a <- plot_brain(imgs$a, legend_title = legend_title, bg_color = 'white', text_color = 'black', ...)
   p$a[[1]] <- p$a[[1]] + labs(title = 'A')
-  p$b <- plot_brain(imgs$b, legend_title = legend_title, ...) + plot_annotation(title = 'B')
+  p$b <- plot_brain(imgs$b, legend_title = legend_title, bg_color = 'white', text_color = 'black', ...) + plot_annotation(title = 'B')
   p$b[[1]] <- p$b[[1]] + labs(title = 'B')
   p$diff <- plot_brain(imgs$diff,
                        fill_mid = 'black',
@@ -37,7 +37,9 @@ compare_brains <- function(a, b, mask, ci = 0.95, legend_title = NULL, out_path 
                        fill_high = 'red',
                        fill_limits = c(-1, 1),
                        legend_position = NULL,
-                       legend_title = legend_title, ...)
+                       legend_title = legend_title,
+                       bg_color = 'white',
+                       text_color = 'black', ...)
   p$diff[[1]] <- p$diff[[1]] + labs(title = 'Difference (B-A)')
   p$compare_brains <- wrap_plots(list(p$a, p$b, p$diff), ncol = 1)
 
