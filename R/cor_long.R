929608d093aadbbe2236165daa09bb8f7d3def0d
Ekarin Eric Pongpipat <epongpipat@gmail.com>
2026-03-10 23:41:09 -0500

    add string pad and preference for importFrom

--- R/cor_long.R
+++ R/cor_long.R
@@ -2,14 +2,24 @@
 #' @concept stats
 #' @param data data to correlate and transform to long format
 #'
-#' @return
+#' @return data.frame
 #' @export
-#' @import dplyr stringr tibble
+#' @import dplyr
+#' @importFrom stringr str_pad
+#' @importFrom stringr str_length
+#' @importFrom stringr str_replace_all
+#' @importFrom stringr str_to_title
+#' @importFrom tibble rownames_to_column
+#' @importFrom tidyr gather
 #' @examples
 #' cor_long(mtcars)
 cor_long <- function(data) {
 
-  colnames(data) <- paste0(1:length(colnames(data)), ". ", colnames(data)) %>%
+  n_pad <- str_pad(string = 1:length(colnames(data)),
+                            width = str_length(length(colnames(data))),
+                            side = 'left',
+                            pad = 0)
+  colnames(data) <- paste0(n_pad, ". ", colnames(data)) %>%
     str_replace_all(., "_", " ") %>%
     str_to_title()
 
