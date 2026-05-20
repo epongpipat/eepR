065e9225339c7ba5aeb0af1f267bd45f1a01c0b4
epongpipat <epongpipat@gmail.com>
2026-05-20 17:27:23 -0500

    update check to look for class rather than length

--- R/models2coefs.R
+++ R/models2coefs.R
@@ -1,5 +1,7 @@
 #' models2coefs
 #'
+#' @description combines model(s) into a single coefficients table. Uses \code{tidy_es()} and adds extra columns of extra model attributes
+#' @concept stats
 #' @param model a single model or a list of models
 #' @param attribute name of attribute list to turn in columns (default: 'extra_info')
 #' @return data.frame of extra_info of model attributes, renamed tidy, and effect size
@@ -8,11 +10,12 @@
 #' @examples
 #' m1 <- lm(salary ~ yrs.since.phd, carData::Salaries)
 #' attr(m1, 'extra_info') <- list(m = 1, another_key = 'another_value')
-#' tidy_extra_info(m1)
+#' models2coefs(m1)
+#' @importFrom dplyr bind_rows
 models2coefs <- function(models, attribute = 'extra_info') {
 
   # checks
-  if (length(models) == 1) {
+  if (!is.null(attr(models, 'class'))) {
     models <- list(models)
   }
 
--- R/models2omni.R
+++ R/models2omni.R
@@ -1,18 +1,22 @@
 #' models2omni
 #'
+#' @description model(s) into a single omnibus table. Uses \code{broom::glance()} and adds extra columns of extra model attributes
+#' @concept stats
 #' @param models a single model or a list of models
 #' @param attribute name of attribute list to turn in columns (default: 'extra_info')
 #' @return data.frame of extra_info of model attributes and glance
 #' @export
 #'
 #' @importFrom broom glance
+#' @importFrom dplyr bind_rows
+#' @examples
 #' m1 <- lm(salary ~ yrs.since.phd, carData::Salaries)
 #' attr(m1, 'extra_info') <- list(m = 1, another_key = 'another_value')
 #' models2omni(m1)
 models2omni <- function(models, attribute = 'extra_info') {
 
   # checks
-  if (length(models) == 1) {
+  if (!is.null(attr(models, 'class'))) {
     models <- list(models)
   }
 
