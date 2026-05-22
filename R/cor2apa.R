a81e1adec1e6ddb43c773a48ab6e45408ab6ab47
Ekarin Eric Pongpipat <epongpipat@gmail.com>
2026-05-21 19:47:41 -0500

    add option for level and format to match related functions

--- R/cor2apa.R
+++ R/cor2apa.R
@@ -2,19 +2,28 @@
 #' @description converts \code{cor.test()} model to APA format string
 #' @concept stats
 #' @param m model from \code{cor.test()}
+#' @param level specify confidence level (default: 0.95). note: level should match the confidence level used in \code{cor.test()}. this argument does not actually update the confidence interval range, but only the reported confidence interval level
 #' @param digits round statistics to specified digit
+#' @param format specify format (default: html) (options: html, plain)
+#'
 #' @returns
 #' @export
 #'
-#' @examples cor2apa(cor.test(carData::Salaries$yrs.since.phd, carData::Salaries$salary))
+#' @examples cor2apa(cor.test(carData::Salaries$yrs.since.phd, carData::Salaries$salary), format = 'plain')
 #' @importFrom broom tidy
 #' @importFrom scales scientific
 #' @importFrom glue glue
-cor2apa <- function(m, digits = 3) {
-  # cor_fit <- cor.test(df_post$pos, df_post$cog)
+cor2apa <- function(m, level = 0.95, digits = 3, format = 'html') {
+
   cor_tidy <- tidy(m)
 
-  r_stat_str <- glue("r({cor_tidy[['parameter']]}) = {round(cor_tidy[['estimate']], 3)}")
+  r <- round(cor_tidy[['estimate']], 3)
+  df_r <- cor_tidy[['parameter']]
+  if (format == 'html') {
+    r_stat_str <- glue("<i>r</i>({df_r}) = {r}")
+  } else if (format == 'plain') {
+    r_stat_str <- glue("r({df_r}) = {r}")
+  }
 
   p <- cor_tidy[['p.value']]
   if (p < .001) {
@@ -22,7 +31,11 @@ cor2apa <- function(m, digits = 3) {
   } else {
     p_str <- sprintf(glue('%.{digits}f'), p)
   }
-  p_str <- glue("p = {p_str}")
+  if (format == 'html') {
+    p_str <- glue("<i>p</i> = {p_str}")
+  } else if (format == 'plain') {
+    p_str <- glue("p = {p_str}")
+  }
 
   ci_ll <- cor_tidy[['conf.low']]
   if (abs(ci_ll) < .001) {
@@ -37,8 +50,10 @@ cor2apa <- function(m, digits = 3) {
   } else {
     ci_ul <- sprintf(glue('%.{digits}f'), ci_ul)
   }
+  ci_str <- glue("{level*100}% CI [{ci_ll}, {ci_ul}]")
 
-  ci_str <- glue("95% CI [{ci_ll}, {ci_ul}]")
   stat_str <- glue("{r_stat_str}, {p_str}, {ci_str}")
+
   return(stat_str)
+
 }
